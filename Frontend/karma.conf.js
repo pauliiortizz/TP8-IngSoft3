134bd1f7486c6e63c6812f3911c7d88ef14298e5
pauliiortizz <2202058@ucc.edu.ar>
2025-11-05 12:18:07 -0300

    se agrego code coverage

--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -9,43 +9,54 @@ module.exports = function (config) {
       require('karma-coverage')
     ],
     reporters: ['progress', 'junit', 'coverage'],
-    coverageReporter: {
-      dir: 'coverage',
-      reporters: [
-        { type: 'html', subdir: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
+
     junitReporter: {
       outputDir: 'test-results',
       outputFile: 'test-results.xml',
       useBrowserName: false
     },
+
+    // ✅ Configuración del reporte de cobertura
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'html', subdir: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov', subdir: '.', file: 'lcov.info' }
+      ]
+    },
+
+    // ✅ Archivos a instrumentar para la cobertura
+    preprocessors: {
+      'src/**/*.ts': ['coverage']
+    },
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
 
-   browsers: ['ChromeHeadlessNoSandbox'],
-customLaunchers: {
-  ChromeHeadlessNoSandbox: {
-    base: 'ChromeHeadless',
-    flags: [
-      '--headless=new',
-      '--no-sandbox',
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--disable-software-rasterizer',
-      '--remote-debugging-port=9222',
-      '--window-size=1920,1080'
-    ]
-  }
-},
-captureTimeout: 300000,
-browserDisconnectTimeout: 300000,
-browserNoActivityTimeout: 300000,
-browserDisconnectTolerance: 5,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--headless=new',
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-setuid-sandbox',
+          '--disable-software-rasterizer',
+          '--remote-debugging-port=9222',
+          '--window-size=1920,1080'
+        ]
+      }
+    },
+
+    captureTimeout: 300000,
+    browserDisconnectTimeout: 300000,
+    browserNoActivityTimeout: 300000,
+    browserDisconnectTolerance: 5,
 
     singleRun: true,
     restartOnFileChange: false
