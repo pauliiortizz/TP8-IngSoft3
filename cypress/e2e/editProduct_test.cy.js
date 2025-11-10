e50e432f2fabecfecc8954eb21993bc8700ff85d
pauliiortizz <2202058@ucc.edu.ar>
2025-11-10 19:46:01 -0300

    listo

--- cypress/e2e/editProduct_test.cy.js
+++ cypress/e2e/editProduct_test.cy.js
@@ -3,27 +3,50 @@ describe('editProductTest', () => {
     // 🌐 Visita la URL del front (usá la de Azure o la local)
     cy.visit('https://productosweb-fpcnfsbsg8gscbat.brazilsouth-01.azurewebsites.net/');
 
-    cy.get('tr:nth-child(2) td:nth-child(5) button.btn i.fa').click();
+    // Esperar a que los productos carguen
+    cy.get('.product-card').should('have.length.at.least', 1);
 
+    // Hacer clic en el botón Editar del primer producto
+    cy.get('.product-card').first().find('.btn-edit').click();
+
+    // Verificar que los campos sean visibles y editables
     cy.get('[name="name"]').should('be.visible').should('not.be.disabled');
     cy.get('[name="stock"]').should('be.visible').should('not.be.disabled');
+    cy.get('[name="price"]').should('be.visible').should('not.be.disabled');
 
     cy.wait(500);
 
-    cy.get('[name="name"]').type('{selectall}{backspace}');
+    // Editar el nombre
+    cy.get('[name="name"]').clear();
     cy.wait(200);
     cy.get('[name="name"]').should('have.value', '');
     cy.get('[name="name"]').type('Pan Integral Modificado');
 
-    cy.get('[name="stock"]').type('{selectall}{backspace}');
+    // Editar el stock
+    cy.get('[name="stock"]').clear();
     cy.wait(200);
     cy.get('[name="stock"]').should('have.value', '');
     cy.get('[name="stock"]').type('56');
 
+    // Editar el precio
+    cy.get('[name="price"]').clear();
+    cy.wait(200);
+    cy.get('[name="price"]').should('have.value', '');
+    cy.get('[name="price"]').type('99.50');
+
+    // Guardar cambios
     cy.get('button.btn-accent').click();
     cy.get('button.btn-primary').click();
 
-    cy.get('tr:nth-child(2) td:nth-child(2)')
-      .should('contain.text', 'Pan Integral MODIFICADO');
+    // Verificar que el producto se actualizó en la lista de cards (case-insensitive)
+    cy.wait(1000); // Esperar a que se actualice la lista
+    cy.get('.product-card').first().find('.product-name')
+      .invoke('text')
+      .should('match', /Pan Integral Modificado/i);
+    cy.get('.product-card').first().find('.stock-value')
+      .should('contain.text', '56');
+    cy.get('.product-card').first().find('.price-value')
+      .invoke('text')
+      .should('match', /99\.50/);
   });
 });
--- cypress/e2e/primer_test.cy.js
+++ cypress/e2e/primer_test.cy.js
@@ -1,14 +1,29 @@
   describe('Mi primera prueba', () => {
    it('Carga correctamente la página de ejemplo', () => {
      cy.visit('https://productosweb-fpcnfsbsg8gscbat.brazilsouth-01.azurewebsites.net/') // Colocar la url local o de Azure de nuestro front
-     cy.get('h4').should('contain', 'Productos') // Verifica que el título contenga "EmployeeCrudAngular"
+     cy.get('h4').should('contain', 'Productos') // Verifica que el título contenga "Productos"
      cy.get('button.btn-corp').click();
      cy.get('[name="name"]').click();
-     cy.get('[name="name"]').type('servilleta');
+     cy.get('[name="name"]').type('TV Samsung - Ultra HD');
      cy.get('[name="stock"]').click();
      cy.get('[name="stock"]').clear();
      cy.get('[name="stock"]').type('8');
+     cy.get('[name="price"]').click();
+     cy.get('[name="price"]').clear();
+     cy.get('[name="price"]').type('599.99');
      cy.get('button.btn-accent').click();
      cy.get('button.btn-primary').click();
+     
+     // Esperar a que aparezcan las cards con el producto creado
+     cy.wait(2000);
+     cy.get('.product-card', { timeout: 10000 }).should('exist').and('be.visible');
+     
+     // Buscar el producto por precio o stock ya que el nombre puede tener transformaciones
+     cy.get('.product-card').should('have.length.at.least', 1);
+     cy.get('.product-card').last().within(() => {
+       cy.get('.product-name').should('be.visible');
+       cy.get('.stock-value').should('contain', '8');
+       cy.get('.price-value').should('contain', '599.99');
+     });
    })
  })
