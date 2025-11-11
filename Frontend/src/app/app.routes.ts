224919e17a69d1436592c0f6d3e323922d01afaa
pauliiortizz <2202058@ucc.edu.ar>
2025-11-11 12:13:24 -0300

    cambios

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { AddproductComponent } from './addproduct/addproduct.component';
-import { productComponent } from './product/product.component';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
   { path: 'addproduct', component: AddproductComponent },
-  { path: '**', component: productComponent },
+  { path: '**', component: ProductComponent },
 ];
